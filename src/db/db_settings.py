bd3e8bd803de0f4b91075749cac000a273e00683
vse-pike <vse.pike@proton.me>
2023-07-05 00:23:17 +0600

    Дебаг джобы №31

--- src/db/db_settings.py
+++ src/db/db_settings.py
@@ -16,7 +16,7 @@ def db_create_engine():
     db_name = os.getenv("POSTGRES_DB")
 
     # Создать строку подключения
-    db_url = f"postgresql+psycopg2://{db_user}:{db_password}@postgres:5432/{db_name}"
+    db_url = f"postgresql+psycopg2://{db_user}:{db_password}@postgres_test:5432/{db_name}"
 
     engine = create_engine(db_url, echo=True)
     m.metadata.create_all(engine)
